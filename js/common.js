105666b398d26a560976e672efb5b1750b66b836
pdxlover <jieunlee0603@gmail.com>
2024-08-19 22:45:40 +0900

    수정
    수정

--- js/common.js
+++ js/common.js
@@ -33,6 +33,45 @@ $(document).ready(function(){//시작
     });
   });//모바일 메뉴 열고 닫기 끝
 
+
+  //스티커 에니메이션 시작
+  gsap.fromTo(".leftright", {
+    rotate: -8,
+  }, {
+    rotate: 8,
+    duration: 1.5,
+    repeat: -1,
+    yoyo: true,
+    ease: 'power2.inOut',
+  });
+
+  gsap.fromTo(".middle", {
+    rotate: 5,
+  }, {
+    rotate: -5,
+    duration: 1.5,
+    repeat: -1,
+    yoyo: true,
+    ease: 'power2.inOut',
+  });
+
+  gsap.fromTo(".rightleft", {
+    rotate: 8,
+  }, {
+    rotate: -8,
+    duration: 1.5,
+    repeat: -1,
+    yoyo: true,
+    ease: 'power2.inOut',
+  });
+   //스티커 에니메이션 끝
+
+
+  //카드 스와이퍼 효과
+   var swiper = new Swiper(".cardSwiper", {
+    effect: "cards",
+    grabCursor: true,
+  });
       
 
     AOS.init();
