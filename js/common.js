1a67e95384aff0a8cc7d6a7d412b02387f086da6
pdxlover <jieunlee0603@gmail.com>
2024-08-27 19:27:12 +0900

    수정
    수정

--- js/common.js
+++ js/common.js
@@ -1,26 +1,53 @@
-$(document).ready(function(){//시작
+$(document).ready(function () {//시작
 
-    //메인비주얼 모바일 스와이퍼 시작
-    var swiper = new Swiper(".m_main_slide",{
-        centeredSlides: true,
-        loop: true,
-        effect:'fade',
-        speed:800,
-        autoplay: {
-          delay: 3000,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      }); //메인비주얼 스와이퍼 끝
+  // //메인비주얼 모바일 스와이퍼 시작
+  // var swiper = new Swiper(".m_main_slide", {
+  //   centeredSlides: true,
+  //   loop: true,
+  //   effect: 'fade',
+  //   speed: 800,
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  // }); //메인비주얼 스와이퍼 끝
 
-        //모바일 메뉴 열고 닫기
+//메인비주얼 텍스트 움직임
+  const targets = gsap.utils.toArray(".split");
+
+  targets.forEach(target => {
+    let SplitClient = new SplitType(target, { type: "lines, words, chars" });
+    let lines = SplitClient.lines;
+    let words = SplitClient.words;
+    let chars = SplitClient.chars;
+
+    gsap.from(chars, {
+      yPercent: 100,
+      autoAlpha: 0,
+      duration: 1,
+      ease: "circ.out",
+      stagger: {
+        amount: 1,
+        from: "random"
+      },
+      scrollTrigger: {
+        trigger: target,
+        start: "top bottom",
+        end: "+=400",
+        markers: false
+      }
+    });
+  });//메인비주얼 텍스트 움직임 끝
+
+  //모바일 메뉴 열고 닫기
   $(".ham").click(function () {
     // $(".dim").fadeIn();
     $(".mgnb_wrap").animate({
@@ -66,15 +93,15 @@ $(document).ready(function(){//시작
     yoyo: true,
     ease: 'power2.inOut',
   });
-   //스티커 에니메이션 끝
+  //스티커 에니메이션 끝
 
 
   //카드 스와이퍼 효과
-   var swiper = new Swiper(".cardSwiper", {
+  var swiper = new Swiper(".cardSwiper", {
     effect: "cards",
     grabCursor: true,
   });
-      
 
-    AOS.init();
+
+  AOS.init();
 });//끝
