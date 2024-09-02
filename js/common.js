6b2ef64e0b59ebf6654346c65e53c3836352210a
pdxlover <jieunlee0603@gmail.com>
2024-09-02 15:03:11 +0900

    수정
    수정

--- js/common.js
+++ js/common.js
@@ -107,10 +107,29 @@ $(document).ready(function () {//시작
 
 
   //카드 스와이퍼 효과
-  var swiper = new Swiper(".cardSwiper", {
-    effect: "cards",
-    grabCursor: true,
-  });
+  // var swiper = new Swiper(".cardSwiper", {
+  //   effect: "cards",
+  //   grabCursor: true,
+  // });
+
+  //메뉴 스와이퍼 시작
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop:true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+  });//메뉴 스와이퍼 끝
 
 
   AOS.init();
